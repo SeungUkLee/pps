551cb62fb252b1bb42896a63d557a10eb440620c
SeungUkLee <lsy931106@gmail.com>
2019-05-17 02:29:42 +0900

    Test, Change: add main test and apply black, isort

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -1,13 +1,17 @@
 import argparse
-import pytest
 import os
 
-from pps.cli import run_pps_cmd, arg_parser, main
+import pytest
+
+from pps.cli import arg_parser, main, run_pps_cmd
+from pps.message import Message
+
+from .mock.data import OPT_SHOW_DATA
 
 
-@pytest.mark.parametrize('arg, namespace', [
-    ('--show', argparse.Namespace(show=True)),
-])
+@pytest.mark.parametrize(
+    'arg, namespace', [('--show', argparse.Namespace(show=True))]
+)
 def test_arg_parser(arg, namespace):
     parser = arg_parser()
     actual = parser.parse_args(arg.split(' '))
@@ -15,46 +19,48 @@ def test_arg_parser(arg, namespace):
     assert actual == namespace
 
 
-@pytest.mark.parametrize('namespace', [
-    argparse.Namespace(show=True),
-    argparse.Namespace(show=False)
-])
+@pytest.mark.parametrize(
+    'namespace',
+    [argparse.Namespace(show=True), argparse.Namespace(show=False)],
+)
 def test_run_cmd_pps(namespace):
     root_dir = os.path.dirname(os.path.abspath(__file__))
     test_file_name = 'test.toml'
     file_path = '{0}/mock/{1}'.format(root_dir, test_file_name)
 
-    opt, res, err = run_pps_cmd(
-        args=namespace,
-        file_path=file_path,
-        test=True
-    )
+    opt, res, err = run_pps_cmd(args=namespace, file_path=file_path, test=True)
 
     if err == -1:
-        pytest.fail("An error occurred while executing script in Pipfile")
+        pytest.fail(Message.EXE_SCRIPT_ERR_MSG)
 
     if opt == 'show':
-        assert res == ['echo: "Echo Hello World!!"', 'version: "python --version"', 'error: "error"']
+        assert res == [
+            'echo: "Echo Hello World!!"',
+            'version: "python --version"',
+            'error: "error"',
+        ]
 
 
-@pytest.mark.parametrize('namespace', [
-    argparse.Namespace(show=True),
-    argparse.Namespace(show=False)
-])
+@pytest.mark.parametrize(
+    'namespace',
+    [argparse.Namespace(show=True), argparse.Namespace(show=False)],
+)
 def test_run_cmd_pps_error(namespace):
     root_dir = os.path.dirname(os.path.abspath(__file__))
     test_file_name = 'no_script_test.toml'
     invalid_file_path = '{0}/mock/{1}'.format(root_dir, test_file_name)
 
     with pytest.raises(SystemExit):
-        run_pps_cmd(
-            args=namespace,
-            file_path='',
-            test=True
-        )
+        run_pps_cmd(args=namespace, file_path='', test=True)
+        run_pps_cmd(args=namespace, file_path=invalid_file_path, test=True)
 
-        run_pps_cmd(
-            args=namespace,
-            file_path=invalid_file_path,
-            test=True
-        )
+
+@pytest.mark.parametrize('arg', ['--show'])
+def test_main(arg, capfd):
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    test_file_name = 'test.toml'
+    file_path = '{0}/mock/{1}'.format(root_dir, test_file_name)
+
+    main(arg, file_path)
+    out, _ = capfd.readouterr()
+    assert out == OPT_SHOW_DATA
